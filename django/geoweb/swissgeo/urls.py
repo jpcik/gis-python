00ec190dfd288ad2f36f72249b95bb9cd8754d90
Jean-Paul Calbimonte <jean-paul.calbimonte@hevs.ch>
2022-04-26 09:16:45 +0200

    updated examples of geo django with docker.

--- django/geoweb/swissgeo/urls.py
+++ django/geoweb/swissgeo/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [    
+
     path('', views.index, name='index'),
     path('city/', views.cities, name='cities'),
     path('city/<int:city_id>', views.city, name='city'), 
-    path('cantons', views.cantons, name='cantons'),
     path('cantons/<str:canton_name>', views.canton, name='canton'),
     path('cantons.json', views.cantonsjson, name='cantonsjson'),
+    path('cantons', views.cantons, name='cantons'),
 ]
--- django/geoweb/swissgeo/views.py
+++ django/geoweb/swissgeo/views.py
@@ -1,37 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import City, Canton
 from django.template import loader
-from .models import City,Canton
 from django.http import Http404
 from django.core.serializers import serialize
 
-# Create your views here.
-from django.http import HttpResponse
 
+# Create your views here.
 def index(request):    
    return HttpResponse("Hi there this is Switzerland")
 
 
 def cities(request): 
-    top_cities=City.objects.order_by('-city_name')[:3] 
-    template= loader.get_template('swissgeo/cities.html')    
-    context = { 'top_cities':top_cities, } 
-    return HttpResponse(template.render(context,request))
+    top_cities=City.objects.order_by('-city_name')[:4] 
     #output = ', '.join([c.city_name for c in top_cities])    
     #return HttpResponse(output)
+    context = { 'top_cities':top_cities, }    
+    return render(request,'swissgeo/index.html',context)
+
 
 def city(request,city_id):    
-    try:         
-        city=City.objects.get(pk=city_id)    
-    except City.DoesNotExist:        
-        raise Http404("City not found!!") 
-    return render(request,'swissgeo/city.html',{'city':city})
+   try:         
+      city=City.objects.get(pk=city_id)    
+   except City.DoesNotExist:        
+      raise Http404("City not found!!") 
+   return render(request,'swissgeo/city.html',{'city':city})
+
 
 def canton(request,canton_name):        
     cantons=Canton.objects.filter(name=canton_name)       
-    #return HttpResponse(cantons[1].geom.wkt)
     return render(request,'swissgeo/canton.html',
-                {'cantonobj':cantons})
-
+                {'cantonobj':cantons[0]})
 
 
 def cantonsjson(request):    
@@ -45,4 +44,3 @@ def cantons(request):
    context ={    }        
    return render(request,
      'swissgeo/cantons.html',context)
-
